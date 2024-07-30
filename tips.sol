117878c3c16d4d7931f3df76c899a9113316408c
Film08 <fim0981346515@gmail.com>
2024-07-30 15:23:40 +0700

    แอด

--- tips.sol
+++ tips.sol
@@ -1,11 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-contract tips{
-
+contract tips {
     address owner;
     constructor(){
         owner = msg.sender;
+        
     }
 
+    //1. put fund in smart contract
+
+    function addtips() payable public  {
+
+    }
+
+    //2. vie balance
+
+    function viewtips() public  view  returns (uint){
+        return address(this).balance;
+    }
+
+    //3. add user
+
+    //4. remove user
+
+    //5. view user
+       
 }
