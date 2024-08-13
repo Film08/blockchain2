f363029c90298b54000fba0e17b9dd464fe88513
Film08 <fim0981346515@gmail.com>
2024-08-13 15:27:47 +0700

    add waitress

--- tips.sol
+++ tips.sol
@@ -3,6 +3,8 @@ pragma solidity 0.8.26;
 
 contract tips {
     address owner;
+    Waitress[] waitress;
+
     constructor(){
         owner = msg.sender;
         
@@ -20,10 +22,31 @@ contract tips {
         return address(this).balance;
     }
 
-    //3. add user
+    //3.1 add structue waitress
+    struct Waitress{
+        address payable walletAddress;
+        string name;
+    }
+    //3.2 add waitress
+    function addWaitress (address payable walletAddress,string memory name) public {
+        bool waitressExist = false;
 
+        if(waitress.length >=1){
+            for (uint i=0; i<waitress.length; i++){
+                if (waitress[i].walletAddress == walletAddress){
+                    waitressExist = true;
+                }
+            }   
+          
+        }
+     if(waitressExist==false){
+        waitress.push(Waitress(walletAddress,name));
+    }
+    }
     //4. remove user
 
-    //5. view user
-       
+    //5. view waitress
+    function viewWaitress() public  view returns (Waitress[]memory) {
+        return  waitress;
+    }
 }
