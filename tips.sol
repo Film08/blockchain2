2d02e2ef7b6cdbad5e366ea7a087a0bc08187338
Film08 <fim0981346515@gmail.com>
2024-09-23 11:58:58 +0700

    Project.git

--- tips.sol
+++ tips.sol
@@ -1,80 +1,53 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
+pragma solidity ^0.8.0;
 
-contract tips {
-    address owner;
-    Waitress[] waitress;
+contract Inheritance {
+    address public owner;
+    uint public totalInheritance;
+    mapping(address => uint) public heirs;
+    address[] public heirsList;
 
-    constructor(){
-        owner = msg.sender;
-        
+    event InheritanceSet(uint amount);
+    event HeirAdded(address heir, uint share);
+    event InheritanceDistributed();
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function.");
+        _;
     }
 
-    //1. put fund in smart contract
-
-    function addtips() payable public  {
-
+    constructor() {
+        owner = msg.sender; // เจ้าของสัญญาคือผู้สร้าง
     }
 
-    //2. vie balance
-
-    function viewtips() public  view  returns (uint){
-        return address(this).balance;
+    function setInheritance(uint _amount) public onlyOwner {
+        totalInheritance = _amount;
+        emit InheritanceSet(_amount);
     }
 
-    //3.1 add structue waitress
-    struct Waitress{
-        address payable walletAddress;
-        string name;
-    }
-    //3.2 add waitress
-    function addWaitress (address payable walletAddress,string memory name) public {
-        require(msg.sender == owner, "Only the owner can call this function");
-        bool waitressExist = false;
+    function addHeir(address _heir, uint _share) public onlyOwner {
+        require(_share > 0, "Share must be greater than zero.");
+        require(heirs[_heir] == 0, "Heir already added.");
 
-        if(waitress.length >=1){
-            for (uint i=0; i<waitress.length; i++){
-                if (waitress[i].walletAddress == walletAddress){
-                    waitressExist = true;
-                }
-            }   
-          
-        }
-     if(waitressExist==false){
-        waitress.push(Waitress(walletAddress,name));
+        heirs[_heir] = _share;
+        heirsList.push(_heir);
+        emit HeirAdded(_heir, _share);
     }
-    }
-    //4. remove user
-    function RemoveWaitress(address payable walletAddress)public{
-        if (waitress.length>1){
-            for(uint i=0; i<waitress.length; i++){
-                if (waitress[i].walletAddress==walletAddress){
-                    for (uint j=i; j<waitress.length-1; j++){
-                        waitress[j]=waitress[j+1];
-                    }
-                    waitress.pop();
-                    break;
-                }
+
+    function distributeInheritance() public onlyOwner {
+        require(totalInheritance > 0, "Total inheritance must be set.");
+
+        for (uint i = 0; i < heirsList.length; i++) {
+            address heir = heirsList[i];
+            uint share = heirs[heir];
+            if (share > 0) {
+                uint amount = totalInheritance * share / 100;
+                payable(heir).transfer(amount);
             }
         }
+        emit InheritanceDistributed();
+        totalInheritance = 0; // รีเซ็ตมรดกหลังการแจกจ่าย
     }
 
-    //5. view waitress
-    function viewWaitress() public  view returns (Waitress[]memory) {
-        return  waitress;
-    }
-    //6.distribute tips
-    function distrubiteTips() public {
-        require(address(this).balance > 0, "Insufficient balance in the contract");
-        if(waitress.length>=1){
-            uint amount = address(this).balance / waitress.length;
-            for (uint i=0; i<waitress.length; i++){
-                transfer(waitress[i].walletAddress,amount);
-            }
-        }
-    }
-    // transfer money
-    function transfer(address payable walletAddress, uint amount) internal {
-        walletAddress.transfer(amount);
-    }
+    receive() external payable {}
 }
